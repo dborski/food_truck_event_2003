e613e5169d80a66da97f1598a7851178a352a6a9
Derek Borski <derek.borski@gmail.com>
2020-04-15 10:32:53 -0600

    Initializes Event class and name attribute

--- test/event_test.rb
+++ test/event_test.rb
@@ -2,51 +2,26 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require "./lib/item"
 require "./lib/food_truck"
+require "./lib/event"
 
 
-class FoodTruckTest < Minitest::Test
+class EventTest < Minitest::Test
 
   def setup
-    @food_truck = FoodTruck.new("Rocky Mountain Pies")
-    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @event = Event.new("South Pearl Street Farmers Market")
   end
 
   def test_it_exists
-    assert_instance_of FoodTruck, @food_truck
+    assert_instance_of Event, @event
   end
 
   def test_it_has_readable_attributes
-    assert_equal "Rocky Mountain Pies", @food_truck.name
+    assert_equal "South Pearl Street Farmers Market", @event.name
   end
 
-  def test_inventory_is_empty_by_default
-    assert_equal ({}), @food_truck.inventory
-  end
 
-  def test_check_stock
-    assert_equal 0, @food_truck.check_stock(@item1)
-  end
-
-  def test_can_stock_item
-    @food_truck.stock(@item1, 30)
-
-    assert_equal ({@item1 => 30}), @food_truck.inventory
-    assert_equal 30, @food_truck.check_stock(@item1)
-
-    @food_truck.stock(@item1, 25)
-
-    assert_equal ({@item1 => 55}), @food_truck.inventory
-    assert_equal 55, @food_truck.check_stock(@item1)
-
-    @food_truck.stock(@item2, 12)
-
-    expected = {@item1 => 55, @item2 => 12}
-    assert_equal expected, @food_truck.inventory
-    assert_equal 55, @food_truck.check_stock(@item1)
-    assert_equal 12, @food_truck.check_stock(@item2)
-  end
 end
+
 
 # ## Iteration 2 - Event and FoodTrucks
 #
