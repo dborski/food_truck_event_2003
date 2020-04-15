83e7311e657666c310c26114c663b8c2a9d1a902
Derek Borski <derek.borski@gmail.com>
2020-04-15 10:38:20 -0600

    Adds add_food_truck method

--- test/event_test.rb
+++ test/event_test.rb
@@ -19,6 +19,28 @@ class EventTest < Minitest::Test
     assert_equal "South Pearl Street Farmers Market", @event.name
   end
 
+  def test_can_add_food_trucks
+    assert_equal [], @event.food_trucks
+
+    @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    @food_truck3.stock(@item1, 65)
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    assert_equal [@food_truck1, @food_truck2, @food_truck3], @event.food_trucks
+  end
+
 
 end
 
@@ -36,23 +58,7 @@ end
 # have that item in stock.
 #
 # Use TDD to create a `Event` class that responds to the following interaction pattern:
-#
-# ```ruby
-# require './lib/item'
-# #=> true
-#
-# require './lib/food_truck'
-# #=> true
-#
-# require './lib/event'
-# #=> true
-#
-# event = Event.new("South Pearl Street Farmers Market")
-# #=> #<Event:0x00007fe134933e20...>
-#
-# event.name
-# #=> "South Pearl Street Farmers Market"
-#
+
 # event.food_trucks
 # #=> []
 #
