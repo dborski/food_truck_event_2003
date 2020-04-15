4a831fd3e6670b9efc7369be861645dc60b0efb7
Derek Borski <derek.borski@gmail.com>
2020-04-15 10:27:30 -0600

    Adds check_stock and stock methods

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,8 +1,19 @@
 class FoodTruck
 
-  attr_reader :name
+  attr_reader :name, :inventory
   def initialize(name)
     @name = name
+    @inventory = Hash.new(0)
+    @item_stock = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @item_stock[item]
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
+    @item_stock[item] += quantity
   end
 
 end
--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -8,6 +8,8 @@ class FoodTruckTest < Minitest::Test
 
   def setup
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   def test_it_exists
@@ -18,46 +20,30 @@ class FoodTruckTest < Minitest::Test
     assert_equal "Rocky Mountain Pies", @food_truck.name
   end
 
+  def test_inventory_is_empty_by_default
+    assert_equal ({}), @food_truck.inventory
+  end
 
+  def test_check_stock
+    assert_equal 0, @food_truck.check_stock(@item1)
+  end
+
+  def test_can_stock_item
+    @food_truck.stock(@item1, 30)
+
+    assert_equal ({@item1 => 30}), @food_truck.inventory
+    assert_equal 30, @food_truck.check_stock(@item1)
+
+    @food_truck.stock(@item1, 25)
+
+    assert_equal ({@item1 => 55}), @food_truck.inventory
+    assert_equal 55, @food_truck.check_stock(@item1)
+
+    @food_truck.stock(@item2, 12)
+
+    expected = {@item1 => 55, @item2 => 12}
+    assert_equal expected, @food_truck.inventory
+    assert_equal 55, @food_truck.check_stock(@item1)
+    assert_equal 12, @food_truck.check_stock(@item2)
+  end
 end
-
-
-# ## Iteration 1 - Items & FoodTrucks
-#
-# The Event will need to keep track of its FoodTrucks and their Items.
-# Each FoodTruck will be able to report its total inventory, stock items,
-# and return the quantity of items. Any item not in stock should return `0` by default.
-#
-# Use TDD to create a `FoodTruck` class that responds to the following interaction pattern:
-
-#
-#food_truck = FoodTruck.new("Rocky Mountain Pies")
-# #=> #<FoodTruck:0x00007f85683152f0...>
-#
-#food_truck.name
-# #=> "Rocky Mountain Pies"
-#
-#food_truck.inventory
-# #=> {}
-#
-#food_truck.check_stock(item1)
-# #=> 0
-#
-#food_truck.stock(item1, 30)
-#
-#food_truck.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 30}
-#
-#food_truck.check_stock(item1)
-# #=> 30
-#
-#food_truck.stock(item1, 25)
-#
-#food_truck.check_stock(item1)
-# #=> 55
-#
-#food_truck.stock(item2, 12)
-#
-#food_truck.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
-# ```
